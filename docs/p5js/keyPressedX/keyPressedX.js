57f9a781dd99c797acdf684bada465ad4d4fa749
jason dykes <j.dykes@city.ac.uk>
2021-09-29 19:16:01 +0100

    Adds KeyPressed & MouseClicked

--- docs/p5js/keyPressedX/keyPressedX.js
+++ docs/p5js/keyPressedX/keyPressedX.js
@@ -1,21 +1,21 @@
+// -- global variable - x position of text
 let x =0;
 
 function setup() {
   createCanvas(400, 400);
   background(220);
   textSize(18);
+  fill(64);
 }
 
+// -- do nothing in th edraw loop for now!
 function draw() {}
 
 function keyPressed() {
-  fill(64);
-  // text(key,x, height/2);
   x+=12;
-  text(key,x%width, height/2 + 24*int(x/width));
-  //  text(key,width/2, height/2);
-  //  text(key,random(20,width-20),random(20,height-20));
-  // text(key, frameCount % width, random(100, height - 100));
+  // -- put the char held in 'key' at a screen position
+  // -- that incresases in x and y with each key press
+  text(key,x%width, 100 + 24*int(x/width));
 }
 
 function mouseClicked() {
