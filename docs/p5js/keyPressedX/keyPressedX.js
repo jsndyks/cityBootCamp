5e865ea37e68a6df2301bc544c63f3f5fcef9b66
jason dykes <j.dykes@city.ac.uk>
2021-09-30 12:18:54 +0100

    Adds space clear

--- docs/p5js/keyPressedX/keyPressedX.js
+++ docs/p5js/keyPressedX/keyPressedX.js
@@ -1,5 +1,5 @@
-// -- global variable - x position of text
-let x =0;
+// -- global variable - used to count keypresses and position text
+let p = 0;
 
 function setup() {
   createCanvas(400, 400);
@@ -8,16 +8,15 @@ function setup() {
   fill(64);
 }
 
-// -- do nothing in th edraw loop for now!
+// -- do nothing in the draw loop for now!
 function draw() {}
 
 function keyPressed() {
-  x+=12;
+  p+=12; // -- add 12 to p each time
   // -- put the char held in 'key' at a screen position
-  // -- that incresases in x and y with each key press
-  text(key,x%width, 100 + 24*int(x/width));
-}
-
-function mouseClicked() {
-  background(220);
+  // --   that increases in x and y with each key press
+  text(key,p%width, 100 + 24*int(p/width));
+  // -- the above just calculates a position for the text as x increases
+  // -- use println() to print out the values to the console if you like\
+  // -- remove '/width' and see what happens as you get to the right hand side!
 }
