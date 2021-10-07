2dc34298dba2ac6589ea7cb02a08f46046ae8c5a
jason dykes <j.dykes@city.ac.uk>
2021-10-07 15:49:40 +0100

    Adds squarism

--- docs/p5js/Squarism/Squarism.js
+++ docs/p5js/Squarism/Squarism.js
@@ -1,5 +1,5 @@
 var img;
-var s = 25;
+var s = 5;
 
 function preload(){
   img = loadImage("data/Holiday.png");
@@ -13,8 +13,10 @@ function setup() {
 }
 
 function draw() {
-  var c = img.get(mouseX, mouseY);
-  fill(c);
-  rect(mouseX, mouseY, s, s);
-  
-}
+  int rx = int(random(-25,25));       // -- integer from -25 to 25 : x offset
+  int ry = int(random(-25,25));       // -- integer from -25 to 25 : y offset
+  let c = img.get(mouseX+rx, mouseY+ry);  // -- called 'img' at cell mouseX,mouseY.
+  fill(c,64);                           // -- Set fill to this semi-opaque colour.
+  rect(mouseX, mouseY, s, s);         // -- Then draw a rectangle at
+}                                       // -- the mouse position.
+
