d040a29440dc75456fff85e71cdcbd974cc987d6
jason dykes <j.dykes@city.ac.uk>
2021-10-07 16:50:09 +0100

    Adds image45_v1

--- docs/p5js/Squarism/Squarism.js
+++ docs/p5js/Squarism/Squarism.js
@@ -1,22 +1,20 @@
-var img;
-var s = 5;
+//let img;
+//let s = 25;
 
-function preload(){
-  img = loadImage("data/Holiday.png");
-}
+//function preload(){
+//  img = loadImage("data/Holiday.png");
+//}
 
 function setup() {
   createCanvas(640, 480);
-  
+  stroke(64);
   rectMode(CENTER);
   background(127);
 }
 
 function draw() {
-  int rx = int(random(-25,25));       // -- integer from -25 to 25 : x offset
-  int ry = int(random(-25,25));       // -- integer from -25 to 25 : y offset
-  let c = img.get(mouseX+rx, mouseY+ry);  // -- called 'img' at cell mouseX,mouseY.
-  fill(c,64);                           // -- Set fill to this semi-opaque colour.
-  rect(mouseX, mouseY, s, s);         // -- Then draw a rectangle at
+  //let c = img.get(mouseX, mouseY);  // -- called 'img' at cell mouseX,mouseY.
+  //fill(c);                           // -- Set fill to this colour.
+  //rect(mouseX, mouseY, s, s);         // -- Then draw a rectangle at
+  rect(mouseX,mouseY,5,5);
 }                                       // -- the mouse position.
-
